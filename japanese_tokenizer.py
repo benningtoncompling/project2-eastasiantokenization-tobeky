8f5cbafa3029e240df2182b78d557288759b2364
Toby Lewkowicz <tobylewkowicz@bennington.edu>
2019-03-18 01:12:31 -0400

    back 2 basics
    we realized that the longer thing did the same thing but slightly worse, so I commented it out and went back to the basics that work without repeating... check out the history of this for all the other ways i got it wrong!!!

--- japanese_tokenizer.py
+++ japanese_tokenizer.py
@@ -14,24 +14,37 @@ lines = text.read().split("\n")
 text.close()
 
 
-# attempt number one
 def max_match(line, dictionary):
-    if line in dictionary and line != "":
-        word = line
-        print(word)
-        if len(line) != len(word):
-            remainder = line[len(word):]
-        else:
-            remainder = ""
-        if remainder == "":
-            return
-        return word + " " + max_match(remainder, dictionary)
+    if line in dictionary:
+        return line + " "
     if len(line) == 1:
-        return str(line) + " "
-    elif len(line) > 1:
+        return line + " "
+    else:
         new_line = line[:-1]
         return max_match(new_line, dictionary)
 
+
+# paulina helped me bang out this bit, but then we realized it was the same as what I had originally
+# sentence = []
+# def max_match(line, dictionary):
+#     if line in dictionary and line != "":
+#         word = line
+#         print(word)
+#         if len(line) != len(word):
+#             remainder = line[len(word):]
+#         else:
+#             remainder = ""
+#         if remainder == "":
+#             sentence.append(word)
+#             return
+#         sentence.append(word)
+#         return word + " " + max_match(remainder, dictionary)
+#     if len(line) == 1:
+#         sentence.append(line)
+#         return str(line) + " "
+#     elif len(line) > 1:
+#         new_line = line[:-1]
+#         return max_match(new_line, dictionary)
 
 result = open("out.txt", "w")
 for line in lines:
@@ -44,6 +57,7 @@ result.close()
 #   Sources:
 #   thanks as always to Sophia for putting up with me/emotional support
 #   kelsey & I worked together on this a bit
+#   paulina also helped!!
 #   thank you Justin for help!
 #   making the txt files into lines: https://stackoverflow.com/questions/14676265/how-to-read-a-text-file-into-a-list-or-an-array-with-python/27046171
 #   inspiration: https://www.youtube.com/watch?v=ABGiqizwCso
